fe60e3e48e41a4dc1f964c0a09ad9ead371190ee
Максим <maks.ohrimenko03032006@gmail.com>
2025-11-17 15:06:25 +0200

    hw9

---
+++ src/main/java/LoginTest.java
@@ -0,0 +1,22 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginTest {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        try {
+            driver.get("https://www.saucedemo.com/");
+            String title = driver.getTitle();
+            if (title.contains("Swag Labs")) {
+                System.out.println("Тест пройшов: заголовок Swag Labs");
+            } else {
+                System.out.println("Тест не пройдено: заголовок не Swag Labs");
+            }
+        } finally {
+            driver.quit();
+        }
+    }
+}
