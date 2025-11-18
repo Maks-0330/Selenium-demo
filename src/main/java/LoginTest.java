ae47a9fa02bc7c7071d46c9bf11f5aba35f718a9
Максим <maks.ohrimenko03032006@gmail.com>
2025-11-18 13:16:20 +0200

    hw10

--- src/main/java/LoginTest.java
+++ src/main/java/LoginTest.java
@@ -1,5 +1,7 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginTest {
@@ -15,7 +17,35 @@ public class LoginTest {
             } else {
                 System.out.println("Тест не пройдено: заголовок не Swag Labs");
             }
-        } finally {//
+
+            //hw10 перевірка елементів
+            try {
+                WebElement usernameImput = driver.findElement(By.id("user-name"));
+                System.out.println("ID: поле логіну знайдено");
+            } catch (Exception e) {
+                System.out.println("Не знайдено ID елемент");
+            }
+
+            try {
+                WebElement loginBtn = driver.findElement(By.className("submit-button"));
+                System.out.println("ClassName: Кнопка логіну знайдене");
+            } catch (Exception e) {
+                System.out.println("Не знайдено ClassName елемент");
+            }
+            try {
+                WebElement credentialsBox = driver.findElement(By.xpath("//div[@id='login_credentials']"));
+                System.out.println("Xpath: Блок логін даних знайдене");
+            } catch (Exception e) {
+                System.out.println("Не знайдено Xpath елемент");
+            }
+
+            try {
+                WebElement passwordInput = driver.findElement(By.cssSelector("input[data-test='password']"));
+                System.out.println("CSS Selector: Поле паролю знайдене");
+            } catch (Exception e) {
+                System.out.println("Не знайдено CSS елемент");
+            }
+        } finally {
             driver.quit();
         }
     }
