7205e1d17cd32b0e610f3bcbaae5c7d34d0353b1
Максим <maks.ohrimenko03032006@gmail.com>
2025-11-22 18:45:43 +0200

    змінив hw10

--- src/main/java/LoginTest.java
+++ src/main/java/LoginTest.java
@@ -17,34 +17,9 @@ public class LoginTest {
             } else {
                 System.out.println("Тест не пройдено: заголовок не Swag Labs");
             }
-
             //hw10 перевірка елементів
-            try {
-                WebElement usernameImput = driver.findElement(By.id("user-name"));
-                System.out.println("ID: поле логіну знайдено");
-            } catch (Exception e) {
-                System.out.println("Не знайдено ID елемент");
-            }
-
-            try {
-                WebElement loginBtn = driver.findElement(By.className("submit-button"));
-                System.out.println("ClassName: Кнопка логіну знайдене");
-            } catch (Exception e) {
-                System.out.println("Не знайдено ClassName елемент");
-            }
-            try {
-                WebElement credentialsBox = driver.findElement(By.xpath("//div[@id='login_credentials']"));
-                System.out.println("Xpath: Блок логін даних знайдене");
-            } catch (Exception e) {
-                System.out.println("Не знайдено Xpath елемент");
-            }
-
-            try {
-                WebElement passwordInput = driver.findElement(By.cssSelector("input[data-test='password']"));
-                System.out.println("CSS Selector: Поле паролю знайдене");
-            } catch (Exception e) {
-                System.out.println("Не знайдено CSS елемент");
-            }
+            WebElement passwordInput = driver.findElement(By.cssSelector("input[data-test='password']"));
+            System.out.println("CSS Selector: Поле паролю знайдене");
         } finally {
             driver.quit();
         }
